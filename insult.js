935f5b442290dff097e817d80e3bb9b534b84566
Tero Ahonen <tahonen@redhat.com>
2016-11-07 17:22:50 +0200

    foobar

--- insult.js
+++ insult.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://insults-be-sake.apps.ocp.ninja/rest/insults"
-    }).then(function(data) {
-       $('.insult-content').append(data.content[0]+' ').append(data.content[1]+' ').append(data.content[2]+'!');
+    $('#insult-button').on('click', function() {
+      $.ajax({
+          url: "http://insults-be-sake.apps.ocp.ninja/rest/insults"
+      }).then(function(data) {
+        var data = 'Thou ' + data.content[0] + ' ' + data.content[1] + ' ' + data.content[2]+ ' ';
+        $('#insult-data').html(data);
+      });
     });
 });
