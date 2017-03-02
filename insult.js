88dcb307170ed7c4929f3d5954026af02a17a859
Tero Ahonen <tero@calevala.com>
2017-03-02 13:12:52 +0200

    Update insult.js

--- insult.js
+++ insult.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://insultsbe:8080/rest/insults"
+        url: "http://insultsbe-dev.52.178.131.150.xip.io/rest/insults"
     }).then(function(data) {
        $('.insult-content').append(data.content[0]+' ').append(data.content[1]+' ').append(data.content[2]+'!');
     });
